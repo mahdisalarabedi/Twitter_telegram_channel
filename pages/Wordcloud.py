0b6412dae8a3054e8a56777b5e01f8c04757e12a
mahdisalarabedi <122230899+mahdisalarabedi@users.noreply.github.com>
2025-05-20 18:17:35 +0330

    Update Wordcloud.py

--- pages/Wordcloud.py
+++ pages/Wordcloud.py
@@ -4,10 +4,13 @@ import streamlit as st
 
 
 # Combine all values into one string
-text = " ".join(st.session_state.outcomes.values())
-
+text_1 = " "
+for i in st.session_state.outcomes.values():
+    for x, y in i.items():
+        if isinstance(y, dict) and 'Category' in y:
+            text_1 += " " + y['Category']
 # Generate the word cloud
-wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text)
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text_1)
 
 # Display in Streamlit
 st.title("Word Cloud")
