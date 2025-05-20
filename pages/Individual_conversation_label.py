dcc1d4760e1442e32f0cb8547fd0055097845f41
mahdisalarabedi <122230899+mahdisalarabedi@users.noreply.github.com>
2025-05-20 18:17:18 +0330

    Update Individual_conversation_label.py

--- pages/Individual_conversation_label.py
+++ pages/Individual_conversation_label.py
@@ -9,11 +9,26 @@ if st.session_state.index < len(keys):
     current_key = keys[st.session_state.index]
     current_value = st.session_state.outcomes[current_key]
     st.markdown(
-                f"""<div style='direction: rtl; text-align: right; font-size:18px;'>
-                {current_key}\n {current_value}</div>
+        f"""<div style='direction: rtl; text-align: right; font-size:18px;'>
+        the tweet is current{current_key} \n
+        </div>
+        """,
+        unsafe_allow_html=True)
+    for x, y in current_value.items():
+        st.markdown(
+                f"""<div style='direction: ltr; text-align: left; font-size:18px;'>
+                the sigfnifier is:\n {x} \n
+                the signifed and the category are:
+                {y}
+                                                </div>
                 """,
                 unsafe_allow_html=True)
+
+
+
+
         # Show button to go to next item
+
     if st.button("➡ Next"):
         st.session_state.index += 1
 else:
