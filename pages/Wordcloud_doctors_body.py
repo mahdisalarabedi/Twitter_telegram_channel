d27fd2e9a2db501dfd78a1e997ba559bb491f4f2
mahdisalarabedi <122230899+mahdisalarabedi@users.noreply.github.com>
2025-05-22 10:32:21 +0330

    Create Wordcloud_doctors_body.py

---
+++ pages/Wordcloud_doctors_body.py
@@ -0,0 +1,22 @@
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+import streamlit as st
+import re
+
+# Combine all values into one string
+text_1 = " "
+for i in st.session_state.outcomes.values():
+    if i[0]['Body']=='doctor body':
+        for y in i[1].values():
+            cleaned_text = re.sub(r'\bbody\b', '', y[1], flags=re.IGNORECASE)
+            text_1 += " " + cleaned_text
+
+# Generate the word cloud
+wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text_1)
+
+# Display in Streamlit
+st.title("Word Cloud")
+fig, ax = plt.subplots()
+ax.imshow(wordcloud, interpolation='bilinear')
+ax.axis("off")
+st.pyplot(fig)
