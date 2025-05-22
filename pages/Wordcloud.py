5451634e6ea4ee39c1ce219abd9e0184abe217b4
mahdisalarabedi <122230899+mahdisalarabedi@users.noreply.github.com>
2025-05-22 10:32:52 +0330

    Update Wordcloud.py

--- pages/Wordcloud.py
+++ pages/Wordcloud.py
@@ -1,14 +1,16 @@
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import streamlit as st
+import re
 
 
 # Combine all values into one string
 text_1 = " "
 for i in st.session_state.outcomes.values():
-    for x, y in i.items():
-        if isinstance(y, dict) and 'Category' in y:
-            text_1 += " " + y['Category']
+    if i[0]['Body']=='patient body':
+        for y in i[1].values():
+               cleaned_text = re.sub(r'\bbody\b', '', y[1], flags=re.IGNORECASE)
+               text_1 += " " + cleaned_text
 # Generate the word cloud
 wordcloud = WordCloud(width=800, height=400, background_color='white').generate(text_1)
 
